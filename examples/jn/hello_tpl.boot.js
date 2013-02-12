8dc9e774498ae55953e78ab15f6bf793498506b7
leeight <leeight@gmail.com>
2013-02-12 16:52:56 +0800

    add more examples

---
+++ examples/jn/hello_tpl.boot.js
@@ -0,0 +1,40 @@
+/***************************************************************************
+ * 
+ * Copyright (c) 2013 Baidu.com, Inc. All Rights Reserved
+ * $Id$ 
+ * 
+ **************************************************************************/
+ 
+ 
+ 
+/**
+ * jn/hello_tpl.boot.js ~ 2013/02/12 14:20:59
+ * @author leeight(liyubei@baidu.com)
+ * @version $Revision$ 
+ * @description 
+ *  
+ **/
+seajs.use(['./hello_tpl'], function(boot){
+  console.log('DONE');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* vim: set ts=4 sw=4 sts=4 tw=100: */
---
+++ examples/jn/hello_tpl.js
@@ -0,0 +1,41 @@
+/***************************************************************************
+ * 
+ * Copyright (c) 2013 Baidu.com, Inc. All Rights Reserved
+ * $Id$ 
+ * 
+ **************************************************************************/
+ 
+ 
+ 
+/**
+ * jn/hello_tpl.js ~ 2013/02/12 14:17:20
+ * @author leeight(liyubei@baidu.com)
+ * @version $Revision$ 
+ * @description 
+ *  
+ **/
+define(function(require, exports, module){
+  var tpl = require('text!./hello_tpl.tpl.html');
+  console.log(tpl);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* vim: set ts=4 sw=4 sts=4 tw=100: */
